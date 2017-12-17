d34575a1656d9e0861492408810f9d1226916469
plooba <league@wts13.attlocal.net>
2017-12-17 11:32:00 -0800

    badger, pizza

---
+++ src/Badger.java
@@ -0,0 +1,33 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Badger {
+public static void main(String[] args) {
+	String b = "badger, ";
+	String a = "snake";
+	String c = "mushroom";
+for (int q = 0; q < 4; q++) {
+
+for (int i = 0; i < 10; i++) {
+System.out.println(b);
+}
+for (int t = 0; t < 2; t++) {
+System.out.println(c);
+}
+System.out.println("a");
+if (q == 3) {	
+	
+System.out.println("African! "+a+", a "+a);	
+System.out.println(a+"! "+"A "+a+", ooh it's a "+a);	
+System.out.println("it's a ");
+for (int y= 0; y < 10; y++) {
+System.out.println(b );
+}
+for (int d = 0; d< 2; d++) {
+	
+System.out.println(c);
+}
+System.out.println("a");
+}
+}
+}
+}
